2ff396541345fac55c249e6278ceed5316519f9a
Jon Chmura <jon.p.chmura@gmail.com>
2018-08-30 14:56:07 -0700

    Initial Commit

---
+++ IFTTT SDK/IFTTT_SDK.h
@@ -0,0 +1,19 @@
+//
+//  IFTTT_SDK.h
+//  IFTTT SDK
+//
+//  Created by Jon Chmura on 8/30/18.
+//  Copyright © 2018 IFTTT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IFTTT_SDK.
+FOUNDATION_EXPORT double IFTTT_SDKVersionNumber;
+
+//! Project version string for IFTTT_SDK.
+FOUNDATION_EXPORT const unsigned char IFTTT_SDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IFTTT_SDK/PublicHeader.h>
+
+
