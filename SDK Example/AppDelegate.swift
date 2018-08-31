41a3b936573bb5c2cd58e58559d323a19fa1a536
Jon Chmura <jon.p.chmura@gmail.com>
2018-08-31 16:38:12 -0700

    Add missing file

---
+++ SDK Example/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SDK Example
+//
+//  Created by Jon Chmura on 8/31/18.
+//  Copyright © 2018 IFTTT. All rights reserved.
+//
+
+import UIKit
+import IFTTT_SDK
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        Applet.Session.shared.serviceId = "lifx"
+//        Applet.Session.shared.userToken = "the_user"
+        
+        return true
+    }
+}
+
