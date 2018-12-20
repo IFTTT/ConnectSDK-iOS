a9501af1ef55574505ac06c33be4bda1c1aad191
Michael Amundsen <michaelamundsen@gmail.com>
2018-12-20 11:09:49 -0500

    Adding tests for isValidEmail

---
+++ IFTTT SDKTests/String+EmailDataDetectorTests.swift
@@ -0,0 +1,28 @@
+//
+//  String+EmailDataDetectorTests.swift
+//  IFTTT SDKTests
+//
+//  Created by Michael Amundsen on 12/20/18.
+//  Copyright © 2018 IFTTT. All rights reserved.
+//
+
+import XCTest
+@testable import IFTTT_SDK
+
+class StringEmailDataDetectorTests: XCTestCase {
+    let emailString = "hello@ifttt.com"
+    let nonEmailString = "Ten minutes left to execute our plan. Where’s everyone else? Playing Spiderman."
+    let blockMixedText = "Ten minutes left to execute our plan. Where’s everyone else? Playing Spiderman. Reach out to hello@ifttt.com"
+    let anotherBlockMixedText = "hello@ifttt.com Ten minutes left to execute our plan. Where’s everyone else? Playing Spiderman."
+    let multipleEmails = "hello@ifttt.com, feedback@ifttt.com"
+    let nonTraditionalEmailAddress = "hello+mike.amu_dsen@ifttt.com"
+    
+    func testIsEmail() {
+        XCTAssertEqual(emailString.isValidEmail, true)
+        XCTAssertEqual(nonEmailString.isValidEmail, false)
+        XCTAssertEqual(blockMixedText.isValidEmail, false)
+        XCTAssertEqual(anotherBlockMixedText.isValidEmail, false)
+        XCTAssertEqual(multipleEmails.isValidEmail, false)
+        XCTAssertEqual(nonTraditionalEmailAddress.isValidEmail, true)
+    }
+}
