5e753dbfbd18a386f29a2131b6b6b7b1124b4adb
Jon Chmura <jon.p.chmura@gmail.com>
2019-04-25 15:14:41 -0400

    Update docs

--- IFTTT SDK/ConnectionCredentialProvider.swift
+++ IFTTT SDK/ConnectionCredentialProvider.swift
@@ -11,16 +11,14 @@ import Foundation
 /// A protocol that defines APIs for providing credentials used during the service authentication process for an `Connection`.
 public protocol ConnectionCredentialProvider {
     
-    /// Provides the OAuth code for your user.
-    /// This code is used to automatically connect your service to IFTTT for the current user when they enable a Connection. This is important so the user is not prompted to sign into your service which would be a poor UX.
+    /// The OAuth code for your user on your service. This is used to skip a step for connecting to your own service during the Connect Button activation flow. We require this value to provide the best possible user experience.
     var oauthCode: String { get }
     
-    /// Provides the user token for the IFTTT user account.
-    /// This is the same token used with the Connection API.
-    /// The IFTTT user token can be obtained on your backend via server to server communication with IFTTT using your IFTTT service key if the user has already connected your service.
-    /// Also, the callback from ConnectButtonController returns the user token when a connection is made.
+    /// This is the IFTTT user token for your service. This token allows you to get IFTTT user data related to only your service. For example, include this token to get the enabled status of Connections for your user. It is also the same token that is used to make trigger, query, and action requests for Connections on behave of the user. You should get this token from a communication between your servers and ours using your `IFTTT-Service-Key`. Never include this key in your app binary, rather create on endpoint on your own server to access the user's IFTTT service token.
+    /// Additionally, the callback from ConnectButtonController returns the user token when a connection is made.
+    /// You should support both method, in the case that your user has already connected your service to IFTTT.
     var userToken: String? { get }
     
-    /// Provides the invite code for testing an unpublished service. If you are developing a service which hasn't been published you can find the invite on code platform.ifttt.com. If your service is published, return nil.
+    /// This value is only required if your service is not published. You can find it on https://platform.ifttt.com on the Service tab in General under invite URL. If your service is published, return nil.
     var inviteCode: String? { get }
 }
