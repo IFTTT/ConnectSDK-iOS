e5a98c83d11d460208d67a923838fcf6789f0f7e
Michael Amundsen <michaelamundsen@gmail.com>
2019-05-28 15:43:43 -0400

    Updating and adding docs

--- IFTTT SDK/ConnectButton+Interaction.swift
+++ IFTTT SDK/ConnectButton+Interaction.swift
@@ -11,11 +11,25 @@ import Foundation
 @available(iOS 10.0, *)
 extension ConnectButton {
     
+    /// A structure to wrap various properties associated with the ability to tap or drag the connect button on and off.
     struct ToggleInteraction {
-        // How easy is it to throw the switch into the next position
+        
+        /// An enum to represent how easy is it to throw the switch into the next position.
         enum Resistance {
-            case light, heavy
             
+            /// A light amount of resistance to flip the switch.
+            case light
+            
+            /// A heavy amount of resistance to flip the switch.
+            case heavy
+            
+            /// Determines if the switch should reverse based on its resistance.
+            ///
+            /// - Parameters:
+            ///   - switchOn: Whether the switch is moving towards being on.
+            ///   - velocity: The value the represents the velocity of the movement towards being on or off.
+            ///   - progress: THe value of how far along the button is to being on or off.
+            /// - Returns: Whether the toggle interaction should be reversed.
             func shouldReverse(switchOn: Bool, velocity: CGFloat, progress: CGFloat) -> Bool {
                 // Negative velocity is oriented towards switch off
                 switch (self, switchOn) {
@@ -34,23 +48,33 @@ extension ConnectButton {
             }
         }
         
-        /// Can the switch be tapped
+        /// Whether the switch can be tapped.
         var isTapEnabled: Bool
         
-        /// Can the switch be dragged
+        /// Whether the switch can be dragged.
         var isDragEnabled: Bool
         
+        /// How easy is it to throw the switch into the next position.
         var resistance: Resistance
         
-        /// What is the next button state when switching the toggle
+        /// An optional next button state when toggling the connect button's switch.
         var toggleTransition: (() -> Transition)?
         
-        /// Callback when switch is toggled
+        /// An optional callback when the switch is flipped.
         var onToggle: (() -> Void)?
         
-        /// Callback when the switch is toggled but we reversed the animation to end in the start position
+        /// An optional callback when the switch is flipped but we reversed the animation to end in the start position.
         var onReverse: (() -> Void)?
         
+        /// Creates a `ToggleInteraction`.
+        ///
+        /// - Parameters:
+        ///   - isTapEnabled: Whether the switch can be tapped. Defaults to false.
+        ///   - isDragEnabled: Whether the switch can be dragged. Defaults to false.
+        ///   - resistance: How easy is it to throw the switch into the next position. Defaults to light.
+        ///   - toggleTransition: An optional next button state when toggling the connect button's switch. Defaults to nil.
+        ///   - onToggle: An optional callback when the switch is flipped. Defaults to nil.
+        ///   - onReverse: An optional callback when the switch is flipped but we reversed the animation to end in the start position. Defaults to nil.
         init(isTapEnabled: Bool = false,
              isDragEnabled: Bool = false,
              resistance: Resistance = .light,
@@ -66,15 +90,20 @@ extension ConnectButton {
         }
     }
     
+    /// Wraps the ability to confirm an email address entry.
     struct EmailInteraction {
-        /// Callback when the email address is confirmed
+        
+        /// An optional callback when the email address is confirmed.
         var onConfirm: ((String) -> Void)?
     }
     
+    /// Wraps the ability to select an object and its action.
     struct SelectInteraction {
+        
+        /// Whether selection is enabled or not.
         var isTapEnabled: Bool = false
         
+        /// An optional callback when the interaction is tapped.
         var onSelect: (() -> Void)?
     }
-    
 }
