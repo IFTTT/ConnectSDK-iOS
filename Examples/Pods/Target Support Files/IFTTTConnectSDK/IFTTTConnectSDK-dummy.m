d67d26a08be3e825e46794e066bbcdd7800d0257
Siddharth Sathyam <siddharth@ifttt.com>
2021-04-26 10:28:42 -0500

    Integrating connect sdk into grocery express using swift package manager instead of cocoapods

--- Examples/Pods/Target Support Files/IFTTTConnectSDK/IFTTTConnectSDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IFTTTConnectSDK : NSObject
-@end
-@implementation PodsDummy_IFTTTConnectSDK
-@end
--- Examples/Pods/Target Support Files/Pods-Grocery Express/Pods-Grocery Express-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Grocery_Express : NSObject
-@end
-@implementation PodsDummy_Pods_Grocery_Express
-@end
